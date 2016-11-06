8226ce7e1cc5bd3e578e14b0b17988a890e5b38d
Emanuel  Rodrigues <emanuelrodrigues@rocketmail.com>
2016-11-06 12:13:59 -0300

    Tela de Cadastro de Cliente

---
+++ src/main/java/projeto/lsi/controller/TelaCadastroDeClientesController.java
@@ -0,0 +1,38 @@
+package projeto.lsi.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class TelaCadastroDeClientesController implements Initializable{
+	
+	  	@FXML
+	    private Button botaoCancelar;
+
+	    @FXML
+	    private Button botaoCadastrar;
+
+	    @FXML
+	    private TextField campoEmailCliente;
+
+	    @FXML
+	    private TextField campoNomeCliente;
+
+	    @FXML
+	    private TextField campoTelefoneCliente;
+
+	    @FXML
+	    private TextField campoCpfCliente;
+
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
+
+}
